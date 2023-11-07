eb54a356a6dffaf5a90cce78819f1e5fc8d12eda
guptaarth87 <guptaarth87@gmail.com>
2023-11-07 22:16:33 +0530

    initial

--- src/Components/Navbar/Header.jsx
+++ src/Components/Navbar/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import { useNavigate } from 'react-router-dom';
+
 export default function Header() {
+ 
+  
+    const navigate = useNavigate();
+  
+    const handleGoBack = () => {
+      navigate(-1);
+    };
+  
   return (
     <>
      <div className = "navbar_custom flex shadow_bottom">
      <div className = "navcontent ">
         Logo
      </div>
+     <button onClick={handleGoBack} className='btn background_clr m-2 button-back' >
+      Go Back
+    </button>
      </div>
     </>
   )
